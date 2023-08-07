e13a8036651710e725d26a53bd0e96f23ed26098
delimmaemma <calvinhtml@gmail.com>
2023-08-06 20:56:33 -0500

    Added word wrapping

--- src/components/Mobile.js
+++ src/components/Mobile.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import '../css/App.css'
 
 const Mobile = () => {
     return (
-        <div>
+        <div className='mobile'>
             <h1>Problem Connecting:</h1>
             <p>Please reload the application and try again if attempting access from a PC or Desktop.</p>
             <p>If attempting access from a mobile device, please be patient. A mobile-friendly application is coming soon.</p>
