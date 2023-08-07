87d636ab7df9d69fe0c639c216fab6c864303477
delimmaemma <calvinhtml@gmail.com>
2023-08-06 20:58:43 -0500

    Added Mobile.js to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,7 @@ import Projects from './Projects';
 import About from './About';
 import Contact from './Contact';
 import SearchBar from './SearchBar';
+import Mobile from './Mobile';
 
 function App() {
 
